492a82bcacf6cea555b7350825a1993057cb221a
Mike Dilger <mike@mikedilger.com>
2022-11-15 17:18:33 +1300

    Switch to hard mode

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
 // Copyright 2015-2020 nostr-proto Developers
 // Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
 // This file may not be copied, modified, or distributed except according to those terms.
+
+#![deny(
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    unused_lifetimes,
+    unused_labels,
+    unused_extern_crates,
+    non_ascii_idents,
+    keyword_idents,
+    deprecated_in_future,
+    unstable_features,
+    single_use_lifetimes,
+    unsafe_code,
+    unreachable_pub,
+    missing_docs,
+    missing_copy_implementations
+)]
