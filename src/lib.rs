d5d826dc3fb985115892695f8b096f7dc29cf6ff
Mike Dilger <mike@mikedilger.com>
2022-11-15 17:16:06 +1300

    Copyright notice

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
-
+// Copyright 2015-2020 nostr-proto Developers
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>
+// This file may not be copied, modified, or distributed except according to those terms.
