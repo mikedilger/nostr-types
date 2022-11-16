df59931f3c80bd0107bd5d9da20aeca5296aed1b
Mike Dilger <mike@mikedilger.com>
2022-11-16 18:14:21 +1300

    Tag, functions for getting fields within

--- src/types/tag.rs
+++ src/types/tag.rs
@@ -1,3 +1,4 @@
+use crate::{Error, Id, PublicKey, Url};
 use serde::de::Error as DeError;
 use serde::de::{Deserialize, Deserializer, SeqAccess, Visitor};
 use serde::ser::{Serialize, SerializeSeq, Serializer};
@@ -18,6 +19,56 @@ pub enum Tag {
 }
 
 impl Tag {
+    /// Get the ID field from an Event Tag
+    pub fn get_id(&self) -> Result<Option<Id>, Error> {
+        if let Tag::Event(v) = self {
+            if let Some(st) = v.get(0) {
+                return Ok(Some(serde_json::from_str(st)?));
+            }
+        }
+        Ok(None)
+    }
+
+    /// Get the URL field from a Tag
+    pub fn get_url(&self) -> Result<Option<Url>, Error> {
+        if let Tag::Event(v) = self {
+            if let Some(u) = v.get(1) {
+                return Ok(Some(Url(u.to_owned())));
+            }
+        }
+        if let Tag::Pubkey(v) = self {
+            if let Some(u) = v.get(1) {
+                return Ok(Some(Url(u.to_owned())));
+            }
+        }
+        Ok(None)
+    }
+
+    /// Get the PublicKey from a Public Key Tag
+    pub fn get_public_key(&self) -> Result<Option<PublicKey>, Error> {
+        if let Tag::Pubkey(v) = self {
+            if let Some(p) = v.get(0) {
+                return Ok(Some(serde_json::from_str(p)?));
+            }
+        }
+        Ok(None)
+    }
+
+    /// Get a String from a Tag at the position `n`
+    pub fn get_string(&self, n: usize) -> Option<String> {
+        if let Tag::Event(v) = self {
+            if let Some(s) = v.get(n) {
+                return Some(s.to_owned());
+            }
+        }
+        if let Tag::Pubkey(v) = self {
+            if let Some(s) = v.get(n) {
+                return Some(s.to_owned());
+            }
+        }
+        None
+    }
+
     // Mock data for testing
     #[allow(dead_code)]
     pub(crate) fn mock() -> Tag {
