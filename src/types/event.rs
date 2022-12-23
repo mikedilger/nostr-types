917c152d13390498bb84d97e8a11a5710a55409f
Mike Dilger <mike@mikedilger.com>
2022-12-24 10:28:51 +1300

    Event query functions that process tags:
    people()
    replies_to()
    reacts_to()
    deletes()
    client()
    subject()
    hashtags()
    urls()
    pow()

--- src/types/event.rs
+++ src/types/event.rs
@@ -1,4 +1,4 @@
-use super::{EventKind, Id, Metadata, PrivateKey, PublicKey, Signature, Tag, Unixtime};
+use super::{EventKind, Id, Metadata, PrivateKey, PublicKey, Signature, Tag, Unixtime, Url};
 use crate::Error;
 use k256::sha2::{Digest, Sha256};
 use serde::{Deserialize, Serialize};
@@ -174,7 +174,32 @@ impl Event {
         Event::new(input, privkey)
     }
 
+    /// If the event refers to people, get all the PublicKeys it refers to
+    /// along with recommended relay URL and petname for each
+    pub fn people(&self) -> Vec<(PublicKey, Option<Url>, Option<String>)> {
+        let mut output: Vec<(PublicKey, Option<Url>, Option<String>)> = Vec::new();
+
+        // All 'p' tags
+        for tag in self.tags.iter() {
+            if let Tag::Pubkey {
+                pubkey,
+                recommended_relay_url,
+                petname,
+            } = tag
+            {
+                output.push((
+                    pubkey.to_owned(),
+                    recommended_relay_url.to_owned(),
+                    petname.to_owned(),
+                ));
+            }
+        }
+
+        output
+    }
+
     /// Is the event a reply?
+    #[deprecated(since = "0.2.0", note = "please use `replies_to` instead")]
     pub fn is_reply(&self) -> bool {
         if self.kind != EventKind::TextNote {
             return false;
@@ -187,6 +212,179 @@ impl Event {
         }
 
         false
+    }
+
+    /// If this event replies to another, get that other event's Id along with
+    /// an optional recommended_relay_url
+    pub fn replies_to(&self) -> Option<(Id, Option<Url>)> {
+        if self.kind != EventKind::TextNote {
+            return None;
+        }
+
+        // look for an 'e' tag with marker 'reply'
+        for tag in self.tags.iter() {
+            if let Tag::Event {
+                id,
+                recommended_relay_url,
+                marker,
+            } = tag
+            {
+                if marker.is_some() && marker.as_deref().unwrap() == "reply" {
+                    return Some((*id, recommended_relay_url.to_owned()));
+                }
+            }
+        }
+
+        // otherwise use the last unmarked 'e' tag
+        for tag in self.tags.iter().rev() {
+            if let Tag::Event {
+                id,
+                recommended_relay_url,
+                marker,
+            } = tag
+            {
+                if marker.is_none() {
+                    return Some((*id, recommended_relay_url.to_owned()));
+                }
+            }
+        }
+
+        None
+    }
+
+    /// If this event reacts to another, get that other event's Id,
+    /// the reaction content, and an optional Recommended relay Url
+    pub fn reacts_to(&self) -> Option<(Id, String, Option<Url>)> {
+        if self.kind != EventKind::Reaction {
+            return None;
+        }
+
+        // The last 'e' tag is it
+        for tag in self.tags.iter().rev() {
+            if let Tag::Event {
+                id,
+                recommended_relay_url,
+                marker: _,
+            } = tag
+            {
+                return Some((*id, self.content.clone(), recommended_relay_url.to_owned()));
+            }
+        }
+
+        None
+    }
+
+    /// If this event deletes others, get all the Ids of the events that it deletes
+    /// along with the reason for the deletion
+    pub fn deletes(&self) -> Option<(Vec<Id>, String)> {
+        if self.kind != EventKind::EventDeletion {
+            return None;
+        }
+
+        let mut ids: Vec<Id> = Vec::new();
+
+        // All 'e' tags are deleted
+        for tag in self.tags.iter() {
+            if let Tag::Event {
+                id,
+                recommended_relay_url: _,
+                marker: _,
+            } = tag
+            {
+                ids.push(*id);
+            }
+        }
+
+        if ids.is_empty() {
+            None
+        } else {
+            Some((ids, self.content.clone()))
+        }
+    }
+
+    /// If this event specifies the client that created it, return that client string
+    pub fn client(&self) -> Option<String> {
+        for tag in self.tags.iter() {
+            if let Tag::Other { tag, data } = tag {
+                if tag == "client" && !data.is_empty() {
+                    return Some(data[0].clone());
+                }
+            }
+        }
+
+        None
+    }
+
+    /// If this event specifies a subject, return that subject string
+    pub fn subject(&self) -> Option<String> {
+        for tag in self.tags.iter() {
+            if let Tag::Subject(sub) = tag {
+                return Some(sub.clone());
+            }
+        }
+
+        None
+    }
+
+    /// Return all the hashtags this event refers to
+    pub fn hashtags(&self) -> Vec<String> {
+        if self.kind != EventKind::TextNote {
+            return vec![];
+        }
+
+        let mut output: Vec<String> = Vec::new();
+
+        for tag in self.tags.iter() {
+            if let Tag::Hashtag(hash) = tag {
+                output.push(hash.clone());
+            }
+        }
+
+        output
+    }
+
+    /// Return all the URLs this event refers to
+    pub fn urls(&self) -> Vec<Url> {
+        if self.kind != EventKind::TextNote {
+            return vec![];
+        }
+
+        let mut output: Vec<Url> = Vec::new();
+
+        for tag in self.tags.iter() {
+            if let Tag::Reference(reference) = tag {
+                output.push(reference.clone());
+            }
+        }
+
+        output
+    }
+
+    /// Get the proof-of-work count of leading bits
+    pub fn pow(&self) -> usize {
+        // Count leading bits in the Id field
+        let mut zeroes: usize = 0;
+        for byte in self.id.0 {
+            if byte == 0 {
+                zeroes += 8;
+            } else {
+                zeroes += byte.leading_zeros() as usize;
+                break;
+            }
+        }
+
+        // Check that they meant it
+        let mut target_zeroes: usize = 0;
+        for tag in self.tags.iter() {
+            if let Tag::Nonce { nonce: _, target } = tag {
+                if let Some(t) = target {
+                    target_zeroes = t.parse::<usize>().unwrap_or(0);
+                }
+                break;
+            }
+        }
+
+        zeroes.max(target_zeroes)
     }
 }
 
