bd134bced3f7a3dfb943cb59403ed02b00522fc2
Mike Dilger <mike@mikedilger.com>
2022-12-28 12:03:56 +1300

    More URL validation

--- src/types/url.rs
+++ src/types/url.rs
@@ -27,15 +27,33 @@ impl Url {
     /// Create a new Url from a string, if it is a valid URL
     pub fn new_validated(s: &str) -> Result<Url, Error> {
         let uri = s.parse::<http::Uri>()?;
-        if let Some(scheme) = uri.scheme() {
-            if scheme.as_str() == "wss" || scheme.as_str() == "ws" {
-                Ok(Url(s.to_owned()))
-            } else {
-                Err(Error::InvalidUrlScheme(scheme.as_str().to_owned()))
-            }
-        } else {
-            Err(Error::MissingUrlScheme)
+
+        let scheme = match uri.scheme() {
+            Some(s) => s,
+            None => return Err(Error::InvalidUrlMissingScheme),
+        };
+
+        if scheme.as_str() != "wss" && scheme.as_str() != "ws" {
+            return Err(Error::InvalidUrlScheme(scheme.as_str().to_owned()));
         }
+
+        let authority = match uri.authority() {
+            Some(a) => a,
+            None => return Err(Error::InvalidUrlMissingAuthority),
+        };
+
+        // FIXME TBD TODO: there are plenty of other invalid ones.
+        let host = authority.host();
+        if host != host.trim()
+            || host.starts_with("localhost")
+            || host.starts_with("127.")
+            || host.starts_with("[::1/")
+            || host.starts_with("[0:")
+        {
+            return Err(Error::InvalidUrlHost(host.to_owned()));
+        }
+
+        Ok(Url(s.to_owned()))
     }
 
     // Mock data for testing
