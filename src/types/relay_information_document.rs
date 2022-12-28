481b31925658646206dd78fdfdc078f70f4651b2
Mike Dilger <mike@mikedilger.com>
2022-12-29 07:12:05 +1300

    impl Display for RelayInformationDocument

--- src/types/relay_information_document.rs
+++ src/types/relay_information_document.rs
@@ -1,5 +1,6 @@
 use super::PublicKeyHex;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 /// Relay information document as described in NIP-11, supplied by a relay
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -37,5 +38,31 @@ impl RelayInformationDocument {
     /// If the relay supports the queried `nip`
     pub fn supports_nip(&self, nip: u32) -> bool {
         self.supported_nips.contains(&nip)
+    }
+}
+
+impl fmt::Display for RelayInformationDocument {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Relay Information:")?;
+        if let Some(name) = &self.name {
+            write!(f, " Name=\"{}\"", name)?;
+        }
+        if let Some(desc) = &self.description {
+            write!(f, " Description=\"{}\"", desc)?;
+        }
+        if let Some(contact) = &self.contact {
+            write!(f, " Contact=\"{}\"", contact)?;
+        }
+        if let Some(pubkey) = &self.pubkey {
+            write!(f, " Pubkey=\"{}\"", pubkey)?;
+        }
+        if let Some(software) = &self.software {
+            write!(f, " Software=\"{}\"", software)?;
+        }
+        if let Some(version) = &self.version {
+            write!(f, " Version=\"{}\"", version)?;
+        }
+        write!(f, " NIPS={:?}", self.supported_nips)?;
+        Ok(())
     }
 }
