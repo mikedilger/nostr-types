c037aad4c37ad200f92bc9db2f1234ea6adcbe7a
Mike Dilger <mike@mikedilger.com>
2023-01-07 15:17:01 +1300

    More functions on Metadata

--- src/types/metadata.rs
+++ src/types/metadata.rs
@@ -5,7 +5,28 @@ use std::collections::HashMap;
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct Metadata(pub HashMap<String, String>);
 
+impl Default for Metadata {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Metadata {
+    /// Create new empty Metadata
+    pub fn new() -> Metadata {
+        Metadata(HashMap::new())
+    }
+
+    /// Get a key's value
+    pub fn get(&self, key: &str) -> Option<String> {
+        self.0.get(key).cloned()
+    }
+
+    /// Set a key's value
+    pub fn set(&mut self, key: String, value: String) {
+        let _ = self.0.insert(key, value);
+    }
+
     // Mock data for testing
     #[allow(dead_code)]
     pub(crate) fn mock() -> Metadata {
@@ -23,9 +44,17 @@ mod test {
     test_serde! {Metadata, test_metadata_serde}
 
     #[test]
-    fn print_metadata_json() {
+    fn test_metadata_print_json() {
         // I want to see if JSON serialized metadata is network appropriate
         let m = Metadata::mock();
         println!("{}", serde_json::to_string(&m).unwrap());
+    }
+
+    #[test]
+    fn test_metadata_get_set() {
+        let mut m = Metadata::new();
+        m.set("name".to_owned(), "Mike".to_owned());
+        assert_eq!(m.get("name"), Some("Mike".to_owned()));
+        assert_eq!(m.get("motorcycle"), None);
     }
 }
