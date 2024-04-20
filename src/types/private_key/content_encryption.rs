e081caf0da113e74ac2eb0c28868097245583566
Mike Dilger <mike@mikedilger.com>
2024-04-20 13:14:50 +1200

    Faster NIP-04 / NIP-44 disambiguation

--- src/types/private_key/content_encryption.rs
+++ src/types/private_key/content_encryption.rs
@@ -54,7 +54,12 @@ impl PrivateKey {
 
     /// Decrypt (detects encryption version)
     pub fn decrypt(&self, other: &PublicKey, ciphertext: &str) -> Result<String, Error> {
-        if ciphertext.contains("?iv=") {
+        let cbytes = ciphertext.as_bytes();
+        if cbytes[ciphertext.len() - 28] == b'?'
+            && cbytes[ciphertext.len() - 27] == b'i'
+            && cbytes[ciphertext.len() - 26] == b'v'
+            && cbytes[ciphertext.len() - 25] == b'='
+        {
             self.decrypt_nip04(other, ciphertext)
                 .map(|bytes| String::from_utf8_lossy(&bytes).into_owned())
         } else {
